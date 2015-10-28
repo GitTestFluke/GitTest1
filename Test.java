1e9316f105a6e3be1f9fb3abca608a7830f65665
hemant kumar verma <hemant.verma@fluke.com>
2015-10-28 15:14:38 +0530

    After uninstall and install testing

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ class Test {
 	public static void main(String[] args) {
 		System.out.println("source tree pull request is not working");
 		System.out.println("first attemp did not work");
+		System.out.println("Uninstalled and installed junk source tree");
 	}
 }
